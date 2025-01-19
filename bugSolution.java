a68429c19fbcedeeccf3f2ed2bdd4a0b257394f5
BugHunter-X <askzenai@gmail.com>
2025-01-20 00:06:05 +0100

    Add bugSolution.java file

---
+++ bugSolution.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        if (0 <= 4 && 4 < arr.length) { 
+            System.out.println(arr[4]); // Accessing a valid element
+        }
+        else{
+            System.out.println("Index out of bound");
+        }
+    }
+}
